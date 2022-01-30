c10433a58b7dd05b5412e0a70daebcde9a0c32ec
cianmung <cianmung1999@gmail.com>
2022-01-30 16:42:41 +0800

    first commit

--- server.js
+++ server.js
@@ -27,10 +27,10 @@ app.use('/paymentdetails', paymentDetailRouter);
 
 if(process.env.NODE_ENV === 'production'){
     //Set static folder
-    app.use(express.static('monthly-grocery-web-app/build'))
+    app.use(express.static('frontend/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'monthly-grocery-web-app', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
 
