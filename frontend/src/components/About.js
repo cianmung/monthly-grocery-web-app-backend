5a3eeedf9fa30c9aafd2bfcdc45c5ef8f4d8e619
cianmung <cianmung1999@gmail.com>
2023-03-26 17:41:51 +0800

    initial commit

--- frontend/src/components/About.js
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return(
-    <div className="About">
-      <h1>About Page</h1>
-    </div>
-  );
-};
-
-export default About;
--- frontend/src/hooks/useSelectedType.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import SelectedTypeContext from "../context/SelectedTypeProvider";
-
-const useSelectedType = () => {
-  return useContext(SelectedTypeContext);
-};
-
-export default useSelectedType;
